7bf1d14da59e14836f135dee7272f411cf75aa79
zika2112 <24.damt.cesaralejandroramos@fundacionmontessori.com>
2025-11-13 16:19:16 +0100

    Se agrega el main pero existen dos fallos en cerrar la app por la ruta y al crear objetos, solucionamos primero cerrar la app

--- src/com/caramos/pizzeriamvc/Principal.java
+++ src/com/caramos/pizzeriamvc/Principal.java
@@ -1,7 +1,13 @@
 package com.caramos.pizzeriamvc;
 
+import com.caramos.pizzeriamvc.gui.PizzeriaControlador;
+import com.caramos.pizzeriamvc.gui.PizzeriaModelo;
+import com.caramos.pizzeriamvc.gui.Ventana;
+
 public class Principal {
     public static void main(String[] args) {
-
+        Ventana vista = new Ventana();
+        PizzeriaModelo modelo = new PizzeriaModelo();
+        PizzeriaControlador controlador = new PizzeriaControlador(vista,modelo);
     }
 }
